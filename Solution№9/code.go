37d0cecd41df6c6dab65630f4b6da51d316cd808
Amir-Samic <samamir060806@gmail.com>
2025-05-24 00:25:08 +0300

    code.go

--- Solution№9/code.go
+++ Solution№9/code.go
@@ -1,72 +1,73 @@
 package main
 
 import (
-	"fmt"
-	"os"
+ "fmt"
+ "os"
 )
 
-//Шифр Атбаш
-func atbash(text string) string {
-	result := ""
-	for _, char := range text {
-		if char >= 'A' && char <= 'Z' {
-			result += string(90 - (char - 65))
-		} else if char >= 'a' && char <= 'z' {
-			result += string(122 - (char - 97))
-		} else {
-			result += string(char)
-		}
-	}
-	return result
+func applyAtbashCipher(originalText string) string {
+ encryptedText := ""
+ for _, character := range originalText {
+  if character >= 'A' && character <= 'Z' {
+   // Преобразование для заглавных букв (A=65, Z=90)
+   encryptedText += string(90 - (character - 65))
+  } else if character >= 'a' && character <= 'z' {
+   // Преобразование для строчных букв (a=97, z=122)
+   encryptedText += string(122 - (character - 97))
+  } else {
+   encryptedText += string(character)
+  }
+ }
+ return encryptedText
 }
 
-//Шифр Цезаря
-func caesar(text string, shift int) string {
-	result := ""
-	for _, char := range text {
-		if char >= 'A' && char <= 'Z' {
-			result += string((int(char)-65+shift)%26 + 65)
-		} else if char >= 'a' && char <= 'z' {
-			result += string((int(char)-97+shift)%26 + 97)
-		} else {
-			result += string(char)
-		}
-	}
-	return result
+func applyCaesarCipher(originalText string, shiftAmount int) string {
+ encryptedText := ""
+ for _, character := range originalText {
+  if character >= 'A' && character <= 'Z' {
+   newPosition := (int(character)-65+shiftAmount)%26 + 65
+   encryptedText += string(newPosition)
+  } else if character >= 'a' && character <= 'z' {
+   newPosition := (int(character)-97+shiftAmount)%26 + 97
+   encryptedText += string(newPosition)
+  } else {
+   encryptedText += string(character)
+  }
+ }
+ return encryptedText
 }
 
-//Главная функция
 func main() {
-	data, err := os.ReadFile("input.txt")
-	if err != nil {
-		fmt.Println("Ошибка чтения файла:", err)
-		return
-	}
-	inputText := string(data)
+ fileContent, err := os.ReadFile("input.txt")
+ if err != nil {
+  fmt.Println("Ошибка при чтении файла:", err)
+  return
+ }
+ textToEncrypt := string(fileContent)
 
-	var shift int
-	fmt.Print("Введите сдвиг для шифра Цезаря: ")
-	_, err = fmt.Scan(&shift)
-	if err != nil {
-		fmt.Println("Ошибка ввода сдвига:", err)
-		return
-	}
+ var shiftValue int
+ fmt.Print("Укажите величину сдвига для шифра Цезаря: ")
+ _, err = fmt.Scan(&shiftValue)
+ if err != nil {
+  fmt.Println("Ошибка при вводе сдвига:", err)
+  return
+ }
 
-	atbashText := atbash(inputText)
-	caesarText := caesar(shift, inputText)
+ atbashEncrypted := applyAtbashCipher(textToEncrypt)
+ caesarEncrypted := applyCaesarCipher(textToEncrypt, shiftValue)
 
-	outputText := fmt.Sprintf(
-		"Атбаш: %s\nЦезарь (сдвиг %d): %s",
-		atbashText,
-		shift,
-		caesarText,
-	)
+ result := fmt.Sprintf(
+  "Атбаш: %s\n Цезаря (сдвиг %d): %s",
+  atbashEncrypted,
+  shiftValue,
+  caesarEncrypted,
+ )
 
-	err = os.WriteFile("output.txt", []byte(outputText), 0644)
-	if err != nil {
-		fmt.Println("Ошибка записи в файл:", err)
-		return
-	}
+ err = os.WriteFile("output.txt", []byte(result), 0644)
+ if err != nil {
+  fmt.Println("Ошибка при записи в файл:", err)
+  return
+ }
 
-	fmt.Println("Готово! Результат записан в output.txt")
+ fmt.Println("Операция завершена! Результаты сохранены в output.txt")
 }
