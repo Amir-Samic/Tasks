ad6eea2816a6c308e9f6b9805d5996ebe1a5d211
Amir-Samic <samamir060806@gmail.com>
2025-05-23 22:28:52 +0300

    code.py

--- Solution№13/code.py
+++ Solution№13/code.py
@@ -1,6 +1,18 @@
+import random
 from collections import Counter
 
-# Чтение чисел из файлов
+#Генерация случайных чисел и запись в файлы
+random.seed(42)
+
+numbers_a = [random.randint(0, 500) for _ in range(1000)]
+with open('A.txt', 'w') as f:
+    f.write(' '.join(map(str, numbers_a)))
+
+numbers_b = [random.randint(200, 700) for _ in range(1000)]
+with open('B.txt', 'w') as f:
+    f.write(' '.join(map(str, numbers_b)))
+
+#Чтение чисел из файлов
 with open('A.txt', 'r') as f:
     numbers_a = list(map(int, f.read().split()))
 
@@ -10,18 +22,19 @@ with open('B.txt', 'r') as f:
 #Находим пересечение множеств
 set_a = set(numbers_a)
 set_b = set(numbers_b)
-intersection = set_a & set_b
+intersection = sorted(set_a & set_b)  # Сразу сортируем
 
 #Подсчитываем вхождения в исходных файлах
 count_a = Counter(numbers_a)
 count_b = Counter(numbers_b)
 
-#Для каждого числа в пересечении находим максимальное количество вхождений
+#Формируем результат с повторениями
 result = []
-for num in sorted(intersection):
+for num in intersection:
     max_count = max(count_a[num], count_b[num])
     result.extend([num] * max_count)
 
 #Записываем результат в файл
 with open('C.txt', 'w') as f:
-    f.write(' '.join(map(str, result)))
+    for i in range(0, len(result), 10):
+        f.write(' '.join(map(str, result[i:i+10])) + '\n')
