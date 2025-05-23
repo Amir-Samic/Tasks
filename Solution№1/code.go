2e8dc1898f4463abae1bd58dba0eb4d12d62c271
Amir-Samic <samamir060806@gmail.com>
2025-05-24 00:07:36 +0300

    code.go

--- Solution№1/code.go
+++ Solution№1/code.go
@@ -1,68 +1,68 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"os"
-	"strconv"
-	"strings"
+ "fmt"
+ "io"
+ "os"
+ "strconv"
+ "strings"
 )
 
-func bubble_sort(arr []int64) {
-	n := len(arr)
-	for i := 0; i < n; i++ {
-		swapped := false
-		for j := 0; j < n-1; j++ {
-			if arr[j] > arr[j+1] {
-				arr[j], arr[j+1] = arr[j+1], arr[j]
-				swapped = true
-			}
-		}
-		if !swapped {
-			break
-		}
-	}
+func sortNumbers(slice []int64) {
+ length := len(slice)
+ for i := 0; i < length; i++ {
+  changed := false
+  for j := 0; j < length-1; j++ {
+   if slice[j] > slice[j+1] {
+    slice[j], slice[j+1] = slice[j+1], slice[j]
+    changed = true
+   }
+  }
+  if !changed {
+   break
+  }
+ }
 }
+
 func main() {
-	input, err := os.Open("input.txt")
+ inputFile, err := os.Open("input.txt")
+ if err != nil {
+  fmt.Println("No input")
+  os.Exit(1)
+ }
 
-	if err != nil {
-		fmt.Println("No input")
-		os.Exit(1)
-	}
+ data := make([]byte, 64)
+ for {
+  _, err := inputFile.Read(data)
+  if err == io.EOF {
+   break
+  }
+ }
 
-	input_data := make([]byte, 64)
-	for {
-		_, err := input.Read(input_data)
-		if err == io.EOF {
-			break
-		}
-	}
+ var numbers []int64
+ for _, part := range strings.Split(string(data), " ") {
+  for _, subpart := range strings.Split(string(part), "\r") {
+   for _, item := range strings.Split(string(subpart), "\x00") {
+    value, _ := strconv.ParseInt(item, 10, 64)
+    if value != 0 {
+     numbers = append(numbers, value)
+    }
+   }
+  }
+ }
+ inputFile.Close()
+ 
+ outputFile, err := os.Create("output.txt")
+ defer outputFile.Close()
 
-	var arr []int64
-	for _, i := range strings.Split(string(input_data), " ") {
-		for _, j := range strings.Split(string(i), "\r") {
-			for _, num := range strings.Split(string(j), "\x00") {
-				tmp, _ := strconv.ParseInt(num, 10, 64)
-				if tmp != 0 {
-					arr = append(arr, tmp)
-				}
-			}
-		}
-	}
-	input.Close()
-	output, err := os.Create("output.txt")
-	defer output.Close()
+ sortNumbers(numbers)
 
-	bubble_sort(arr)
+ result := ""
+ for _, num := range numbers {
+  result += fmt.Sprintf("%d ", num)
+ }
 
-	output_str := ""
-	for _, i := range arr {
-		output_str += fmt.Sprintf("%d ", i)
-	}
+ outputFile.WriteString(result)
 
-	output.WriteString(output_str)
-
-	fmt.Println(arr)
-
+ fmt.Println(numbers)
 }
